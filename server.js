a3fce0d3d011c4e6380d0e08edf4aa681ab81d49
DanielLopez199920 <137942091+DanielLopez199920@users.noreply.github.com>
2025-03-20 00:11:54 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,27 +2,33 @@ const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
 const athleteRoutes = require("./routes/athletes");
-const userRoutes = require("./routes/users"); // 🔹 Agregado
+const userRoutes = require("./routes/users");
 const path = require("path");
 const app = express();
 const visitRoutes = require("./routes/visits");
+
 // 🔹 Middleware
 app.use(cors({
   origin: "https://monumental-wisp-90771f.netlify.app",
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
+
+// 🔥 SOLUCIÓN CLAVE PARA PREVENT CORS ERRORS
+app.options("*", cors());
+
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads"))); // ⚠️ Esto es importante
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 // 🔹 Rutas
 app.use("/api/auth", authRoutes);
 app.use("/api/athletes", athleteRoutes);
-app.use("/api/users", userRoutes); // 🔹 Nueva ruta
+app.use("/api/users", userRoutes);
 app.use("/api/visits", visitRoutes);
+
 // 🔹 Iniciar Servidor
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🔥 Servidor corriendo en http://localhost:${PORT}`);
 });
