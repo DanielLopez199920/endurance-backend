2c2f255a0461b166e62f3c747b032ea4c20972a6
DanielLopez199920 <137942091+DanielLopez199920@users.noreply.github.com>
2025-04-02 19:10:03 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware CORS robusto
 app.use(cors({
-  origin: "https://monumental-wisp-90771f.netlify.app",
+  origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
