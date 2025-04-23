e6c4d50a9bcfabb99495ddc04bf4722d0bdc4425
DanielLopez199920 <137942091+DanielLopez199920@users.noreply.github.com>
2025-04-23 08:30:39 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,29 +9,28 @@ const visitRoutes = require("./routes/visits");
 
 const app = express();
 
-// Middleware CORS robusto
-app.use(cors({
-  origin: "https://monumental-wisp-90771f.netlify.app",
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
-}));
+const allowedOrigins = [
+  "https://astounding-nasturtium-63d933.netlify.app",
+  "http://localhost:3000"
+];
 
-// Preflight para todas las rutas
+app.use(cors()); 
+
+
 app.options("*", cors());
 
 app.use(express.json());
 
-// Servir imágenes
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Rutas API
+
 app.use("/api/auth", authRoutes);
 app.use("/api/athletes", athleteRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/visits", visitRoutes);
 
-// Iniciar servidor
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🔥 Servidor corriendo en puerto ${PORT}`);
